be9a4c5d3a0ac7334eda9e600590032057a63486
ImNotAVirus <s.soulie@hotmail.fr>
2019-03-12 10:57:51 +0100

    :art: Rename processes

--- apps/login_server/lib/login_server.ex
+++ apps/login_server/lib/login_server.ex
@@ -8,7 +8,7 @@ defmodule LoginServer do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      {LoginServer.Frontend, name: LoginServer}
+      {LoginServer.Frontend, name: LoginServer.Frontend}
     ]
 
     opts = [strategy: :one_for_one, name: LoginServer.Supervisor]
