e552ba6131d022e9b809cc0f7bfbd47f17bede8b
ImNotAVirus <s.soulie@hotmail.fr>
2019-06-29 14:18:05 +0200

    :sparkles: Now can start applications alone

--- apps/login_server/mix.exs
+++ apps/login_server/mix.exs
@@ -5,6 +5,10 @@ defmodule LoginServer.MixProject do
     [
       app: :login_server,
       version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
--- apps/world_manager/mix.exs
+++ apps/world_manager/mix.exs
@@ -5,6 +5,10 @@ defmodule WorldManager.MixProject do
     [
       app: :world_manager,
       version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
