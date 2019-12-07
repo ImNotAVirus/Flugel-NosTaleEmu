ed7a9377cc857ce8e1ebe0407a026cd0e12cf748
ImNotAVirus <s.soulie@hotmail.fr>
2019-12-07 21:38:39 +0100

    :sparkles: Add unique index on username column

--- apps/database_service/priv/repo/migrations/20191207124226_create_accounts.exs
+++ apps/database_service/priv/repo/migrations/20191207124226_create_accounts.exs
@@ -7,12 +7,14 @@ defmodule DatabaseService.Repo.Migrations.CreateAccounts do
     LanguageKey.create_type()
 
     create table(:accounts) do
-      add :name, :string, null: false
+      add :username, :string, null: false
       add :password, :string, null: false
       add :authority, :integer, default: 0, null: false
       add :language, LanguageKey.type(), default: "en", null: false
 
       timestamps()
     end
+
+    create unique_index(:accounts, [:username])
   end
 end
